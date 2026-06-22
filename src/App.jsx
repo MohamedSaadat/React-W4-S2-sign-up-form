63748e60259f5460aa72bd2ca8b559a0d3b7f6c7
MohamedSaatat <mohamedgitsaadat@gmail.com>
2026-06-23 01:34:39 +0300

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,101 +1,39 @@
 import React from "react";
-import {
-  HeroUIProvider,
-  Navbar,
-  NavbarBrand,
-  NavbarMenuToggle,
-  NavbarMenu,
-  NavbarMenuItem,
-  NavbarContent,
-  NavbarItem,
-  Link,
-  Button,
-} from "@heroui/react";
-import AcmeLogo from "./AcmeLogo";
+import "./App.css";
+import { HeroUIProvider } from "@heroui/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./Layouts/MainLayout";
+import Feed from "./pages/feed";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
+import AuthLayout from "./Layouts/AuthLayout";
 
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: <MainLayout />,
+    children: [
+      { index: true, element: <Feed /> },
+      { path: "profile", element: <Profile /> },
+      { path: "*", element: <NotFound /> },
+    ],
+  },
+  {
+    path: "",
+    element: <AuthLayout />,
+    children: [
+      { path: "signin", element: <SignIn /> },
+      { path: "signup", element: <SignUp /> },
+    ],
+  },
+]);
 export default function App() {
-  const menuItems = [
-    "Profile",
-    "Dashboard",
-    "Activity",
-    "Analytics",
-    "System",
-    "Deployments",
-    "My Settings",
-    "Team Settings",
-    "Help & Feedback",
-    "Log Out",
-  ];
   return (
     <>
       <HeroUIProvider>
-        <Navbar disableAnimation isBordered>
-          <NavbarContent className="sm:hidden" justify="start">
-            <NavbarMenuToggle />
-          </NavbarContent>
-
-          <NavbarContent className="sm:hidden pr-3" justify="center">
-            <NavbarBrand>
-              <AcmeLogo />
-              <p className="font-bold text-inherit">ACME</p>
-            </NavbarBrand>
-          </NavbarContent>
-
-          <NavbarContent className="hidden sm:flex gap-4" justify="center">
-            <NavbarBrand>
-              <AcmeLogo />
-              <p className="font-bold text-inherit">ACME</p>
-            </NavbarBrand>
-            <NavbarItem>
-              <Link color="foreground" href="#">
-                Features
-              </Link>
-            </NavbarItem>
-            <NavbarItem isActive>
-              <Link aria-current="page" color="warning" href="#">
-                Customers
-              </Link>
-            </NavbarItem>
-            <NavbarItem>
-              <Link color="foreground" href="#">
-                Integrations
-              </Link>
-            </NavbarItem>
-          </NavbarContent>
-
-          <NavbarContent justify="end">
-            <NavbarItem className="hidden lg:flex">
-              <Link href="#">Login</Link>
-            </NavbarItem>
-            <NavbarItem>
-              <Button as={Link} color="warning" href="#" variant="flat">
-                Sign Up
-              </Button>
-            </NavbarItem>
-          </NavbarContent>
-
-          <NavbarMenu>
-            {menuItems.map((item, index) => (
-              <NavbarMenuItem key={`${item}-${index}`}>
-                <Link
-                  className="w-full"
-                  color={
-                    index === 2
-                      ? "warning"
-                      : index === menuItems.length - 1
-                        ? "danger"
-                        : "foreground"
-                  }
-                  href="#"
-                  size="lg"
-                >
-                  {item}
-                </Link>
-              </NavbarMenuItem>
-            ))}
-          </NavbarMenu>
-        </Navbar>
-        <h1 class="text-3xl font-bold underline">Hello world!</h1>
+        <RouterProvider router={router} />
       </HeroUIProvider>
     </>
   );
